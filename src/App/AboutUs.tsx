38e7f36b7c2963c0f52e033f646dbccca80504d0
chorisonobeer <152948858+chorisonobeer@users.noreply.github.com>
2023-12-11 12:20:19 +0900

    Update AboutUs.tsx

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -18,157 +18,157 @@ const Content = () => {
           <div className="logo">佐和田料飲店組合MAP2024</div>
         </div>
 
-   <div class="map2024">佐和田料飲店組合MAP2024</div>
-      <div class="map-container">
-        <p class="map">
+   <div className="map2024">佐和田料飲店組合MAP2024</div>
+      <div className="map-container">
+        <p className="map">
           <b>おでわんMAPとは</b>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <b>&nbsp;</b>
         </p>
-        <p class="blank-line">🐾 愛犬と一緒に入れる飲食店</p>
-        <p class="blank-line">🐾 愛犬と泊まれるホテル・宿</p>
-        <p class="blank-line">🐾 ドッグラン</p>
-        <p class="blank-line">
+        <p className="blank-line">🐾 愛犬と一緒に入れる飲食店</p>
+        <p className="blank-line">🐾 愛犬と泊まれるホテル・宿</p>
+        <p className="blank-line">🐾 ドッグラン</p>
+        <p className="blank-line">
           カテゴリの施設情報を地図上に表示しています（全国版）
         </p>
-        <p class="blank-line">&nbsp;</p>
-        <p class="blank-line">
+        <p className="blank-line">&nbsp;</p>
+        <p className="blank-line">
           最大の特徴は実際におでかけしてきた飼い主様からの感想や写真で構成しているところ！
         </p>
-        <p class="blank-line">&nbsp;</p>
-        <p class="blank-line">掲載している施設数は1400件超！</p>
-        <p class="blank-line">（2023年1月現在）</p>
-        <p class="blank-line">
+        <p className="blank-line">&nbsp;</p>
+        <p className="blank-line">掲載している施設数は1400件超！</p>
+        <p className="blank-line">（2023年1月現在）</p>
+        <p className="blank-line">
           2019年のおでかけわんこ部運営開始から毎日たくさんのおでかけ情報や写真を提供頂いております。
         </p>
-        <p class="blank-line">&nbsp;</p>
-        <p class="blank-line">
+        <p className="blank-line">&nbsp;</p>
+        <p className="blank-line">
           おでわんMAPは全国のわんちゃんのおでかけの思い出がたっぷり詰まった愛情たっぷりのMAPです。
         </p>
-        <p class="blank-line">&nbsp;</p>
-        <p class="map">
+        <p className="blank-line">&nbsp;</p>
+        <p className="map">
           <b>おでわんMAPでできること</b>
         </p>
       </div>
-      <div class="div">
-        <p class="blank-line">
+      <div className="div">
+        <p className="blank-line">
           01. 全国の愛犬と一緒に行けるスポットを地図で探せる
         </p>
-        <p class="blank-line">02. アイコンをタップするとさらに詳細ページへ</p>
-        <p class="blank-line">
+        <p className="blank-line">02. アイコンをタップするとさらに詳細ページへ</p>
+        <p className="blank-line">
           03. 探し方色々！カテゴリで絞り込み検索や現在位置から近い順で探せる
         </p>
       </div>
-      <div class="map-map">
-        <p class="blank-line">
-          <b class="map4">おでわんMAPを使う方法は？</b>
+      <div className="map-map">
+        <p className="blank-line">
+          <b className="map4">おでわんMAPを使う方法は？</b>
         </p>
-        <p class="blank-line">
-          <b class="map4">&nbsp;</b>
+        <p className="blank-line">
+          <b className="map4">&nbsp;</b>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span
             >「おでわんMAP」はおでかけわんこ部メールマガジン（無料部員）への登録で無料で利用可能です。
           </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>メールマガジン登録フォームは </span>
-          <a class="a" href="https://form.run/@odewan-member" target="_blank">
+          <a className="a" href="https://form.run/@odewan-member" target="_blank">
             <span>
-              <span class="span">こちら</span>
+              <span className="span">こちら</span>
             </span>
           </a>
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>おでかけわんこ部とは </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>愛犬とのおでかけ情報を発信しているメディアです。 </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>「週末このカフェに行ってみようか?」 </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>「この景色を一緒に見にいこう!!」 </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span
             >愛犬との楽しいおでかけのきっかけ作りサポートし「飼い主さんとわんこをHAPPYにする」ことが目的です。
           </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>webサイト: </span>
-          <a class="a" href="https://odekake-wanko-bu.com/" target="_blank">
+          <a className="a" href="https://odekake-wanko-bu.com/" target="_blank">
             <span>
-              <span class="span">https://odekake-wanko-bu.com</span>
+              <span className="span">https://odekake-wanko-bu.com</span>
             </span>
           </a>
         </p>
-        <p class="blank-line">
-          <a class="a" href="https://odekake-wanko-bu.com/" target="_blank">
+        <p className="blank-line">
+          <a className="a" href="https://odekake-wanko-bu.com/" target="_blank">
             <span>
-              <span class="span">&nbsp;</span>
+              <span className="span">&nbsp;</span>
             </span>
           </a>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>Instagram: </span>
           <a
-            class="a"
+            className="a"
             href="https://www.instagram.com/odewanbu/"
             target="_blank"
           >
             <span>
-              <span class="span">@odewanbu</span>
+              <span className="span">@odewanbu</span>
             </span>
           </a>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>おでかけレポート募集中 </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span
             >あなたの大切な愛犬とのおでかけの思い出を全国の飼い主さまにもシェアしませんか？
           </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span>&nbsp; </span>
         </p>
-        <p class="blank-line">
+        <p className="blank-line">
           <span
             >＼こんな情報を募集しております／「愛犬と入れるカフェ」「愛犬と泊まれる宿」「ドッグラン」
           </span>
         </p>
       </div>
-      <div class="parent">
-        <img class="icon" alt="" src="./public/@2x.png" />
+      <div className="parent">
+        <img className="icon" alt="" src="./public/@2x.png" />
 
-        <img class="icon1" alt="" src="./public/1@2x.png" />
+        <img className="icon1" alt="" src="./public/1@2x.png" />
 
-        <img class="icon2" alt="" src="./public/2@2x.png" />
+        <img className="icon2" alt="" src="./public/2@2x.png" />
 
-        <img class="icon3" alt="" src="./public/3@2x.png" />
+        <img className="icon3" alt="" src="./public/3@2x.png" />
 
-        <img class="icon4" alt="" src="./public/4@2x.png" />
+        <img className="icon4" alt="" src="./public/4@2x.png" />
 
-        <img class="icon5" alt="" src="./public/5@2x.png" />
+        <img className="icon5" alt="" src="./public/5@2x.png" />
 
       </div> 
         
